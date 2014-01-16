51c8333a0e5d4249a0c5212944df1091a05223d4
Valentin Simonov <v@lent.in>
2014-01-16 15:13:12 +0400

    Removed Unity 4.3 stuff for TouchScript to work in Unity 4.2.

--- TouchScript/Hit/TouchHit.cs
+++ TouchScript/Hit/TouchHit.cs
@@ -31,31 +31,14 @@ namespace TouchScript.Hit
             return result;
         }
 
-        public static TouchHit FromRaycastHit2D(RaycastHit2D value)
-        {
-            var result = new TouchHit()
-            {
-                Type = HitType.Hit2D,
-                Collider2d = value.collider,
-                Distance = value.fraction,
-                Normal = -Vector3.forward,
-                Point = value.point,
-                Rigidbody2D = value.rigidbody,
-                Transform = value.collider.transform
-            };
-            return result;
-        }
-
         public HitType Type;
         public Vector3 BarycentricCoordinate;
         public Collider Collider;
-        public Collider2D Collider2d;
         public float Distance;
         public Vector2 LightmapCoord;
         public Vector3 Normal;
         public Vector3 Point;
         public Rigidbody Rigidbody;
-        public Rigidbody2D Rigidbody2D;
         public Vector2 TextureCoord;
         public Vector2 TextureCoord2;
         public Transform Transform;
